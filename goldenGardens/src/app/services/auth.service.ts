90aed05fa350e27ae926a1b3763d9dda413ec344
JHNUXER <johnhoganuke2@gmail.com>
2019-04-03 16:00:01 -0400

    Lots of changes, fixed index to not corrupt other views

---
+++ goldenGardens/src/app/services/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  login(loginInfo) {
+    const str = `grant_type=password&username=${encodeURI(loginInfo.email)}&password=${encodeURI(loginInfo.password)}`;
+    
+  }
+}
