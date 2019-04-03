687560553fc16dcb894a3014d70d5ee8e38de8ed
lisajkilgore <lisajkilgore@gmail.com>
2019-04-03 16:42:33 -0400

    contact form

--- goldenGardens/src/app/app-routing.module.ts
+++ goldenGardens/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { IndexComponent } from './index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: IndexComponent},
+  {path: 'about', component: AboutComponent }
+];
 
 @NgModule({
+  declarations: [
+    AboutComponent,
+  ],
+  
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+
 })
 export class AppRoutingModule { }
